11cbd0009bec85301a260c8f6f1141200cf1a35b
maciej-debowski <66415135+maciej-debowski@users.noreply.github.com>
2020-08-21 21:23:14 +0200

    Update layer.php

--- layer.php
+++ layer.php
@@ -5,7 +5,7 @@ class qa_html_theme_layer extends qa_html_theme_base
     function head_script()
     {
         if (qa_get_logged_in_userid()) {
-            $this->content['script'][] = '<script src="' . qa_html(QA_HTML_THEME_LAYER_URLTOROOT . 'js/veryfied.js') . '"></script>';
+            $this->content['script'][] = '<script src="' . qa_html(QA_HTML_THEME_LAYER_URLTOROOT . 'veryfied.js') . '"></script>';
         }
         qa_html_theme_base::head_script();
     }
